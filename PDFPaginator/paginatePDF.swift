7b717330a7ab0987eb4540ea464cd2ade9e39a59
Paul Gowder <paul.gowder@gmail.com>
2019-06-04 22:53:46 -0500

    can do multiple pages

--- PDFPaginator/paginatePDF.swift
+++ PDFPaginator/paginatePDF.swift
@@ -45,6 +45,23 @@ func writeOnPage(doc: PDFDocument, page: Int) -> PDFDocument {
     return outDocument
 }
 
+func mergePDFs(pdfs: [PDFDocument]) -> PDFDocument {
+    let first = pdfs[0]
+    let rest = pdfs[1...]
+    var curpagenum = first.pageCount
+    var curpage: PDFPage
+    var lenOfCurAdd: Int
+    for p2add in rest {
+        lenOfCurAdd = p2add.pageCount
+        for i in 0..<lenOfCurAdd {
+            curpage = p2add.page(at: i)!
+            first.insert(curpage, at: curpagenum)
+            curpagenum+=1
+        }
+    }
+    return first
+}
+
 func makePDFArray(_ inUrl: URL){
     var outArray = [PDFDocument]()
     let doc: PDFDocument = PDFDocument(url: inUrl)!
@@ -54,4 +71,7 @@ func makePDFArray(_ inUrl: URL){
     }
     let outUrl = URL(fileURLWithPath: "/Users/pauliglot/Downloads/testout.pdf")
     outArray.last!.write(to: outUrl)
+    let otherOutUrl = URL(fileURLWithPath: "/Users/pauliglot/Downloads/othertestout.pdf")
+    let wholeDoc = mergePDFs(pdfs: outArray)
+    wholeDoc.write(to: otherOutUrl)
 }
