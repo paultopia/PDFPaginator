9bd82edec3b2632646885172ad3d6e9300baecde
Paul Gowder <paul.gowder@gmail.com>
2019-06-04 21:15:21 -0500

    Initial Commit

---
+++ PDFPaginator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PDFPaginator
+//
+//  Created by Paul Gowder on 6/4/19.
+//  Copyright © 2019 Paul Gowder. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
